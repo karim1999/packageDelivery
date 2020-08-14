b39327674bee9b7315164f48da4992ed6b4589ca
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-08-14 13:35:49 +0200

    Passport and Cashier

--- app/User.php
+++ app/User.php
@@ -5,10 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Billable, HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
